9e5c5da5bb6d6062725f2615bc4786fa4aad6f73
JIHANE241 <belboujihane@gmail.com>
2026-03-29 16:51:47 +0100

    fix factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Str;
  */
 class UserFactory extends Factory
 {
+    protected $model = \App\Models\User::class;
     /**
      * The current password being used by the factory.
      */
@@ -25,12 +26,12 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
-        ];
+        'name' => $this->faker->name(),
+        'email' => $this->faker->unique()->safeEmail(),
+        'email_verified_at' => now(),
+        'password' => static::$password ??= \Illuminate\Support\Facades\Hash::make('password'),
+        'remember_token' => \Illuminate\Support\Str::random(10),
+    ];
     }
 
     /**
